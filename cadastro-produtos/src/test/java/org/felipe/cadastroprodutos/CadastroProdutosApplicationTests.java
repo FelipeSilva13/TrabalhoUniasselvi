875b4bda84c403fd3b609f7bc5c2e03582b73471
Felipe Silva <felipyago@outlook.com>
2025-06-28 00:20:09 -0300

    primeiro dia de trabalho

---
+++ cadastro-produtos/src/test/java/org/felipe/cadastroprodutos/CadastroProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.felipe.cadastroprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroProdutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
